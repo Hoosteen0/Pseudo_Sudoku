6eaf3a0d36e71fd6ef431acbf5bf4ef572c28715
Hoosteen0 <113567993+Hoosteen0@users.noreply.github.com>
2023-02-08 00:25:04 -0500

    Delete Node.py

--- Node.py
+++
@@ -1,9 +0,0 @@
-# imports
-
-class Node:
-    def __init__(self, value, n):
-        self._data = value
-        self._children = [range(1, n + 1)]
-
-    def update_value(self, new_value):
-        self._data = new_value
